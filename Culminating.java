09b568297f1dbe6f7a2fd1df0ae0225eb5c6d32b
AlexanderAuton <350319867@tdsb.ca>
2026-06-09 15:38:16 +0000

    V5 (Yes, i know it looks bad but its the same code under the other assignment)

---
+++ Culminating.java
@@ -0,0 +1,83 @@
+/**
+
+        * File: Culminating
+
+        * Author: Alexander D. Auton
+
+        * Date Created: June 02, 2026
+
+        * Date Last Modified: June --, 2026
+
+        */
+        import java.util.Scanner;
+        import java.util.HashMap;
+        import javafx.application.Application;
+        import javafx.scene.Scene;
+        import javafx.scene.layout.StackPane;
+        import javafx.stage.Stage;
+        import javafx.scene.text.Text;
+
+        public class Culminating extends Application {
+	   //and so it begins
+public void start(Stage campbellyFeud){
+
+        Text y = new Text("y");
+
+        Scene x = new Scene(y,100,100);
+        campbellyFeud.setScene(x);
+        campbellyFeud.show();
+
+                }
+        public static void main(String[] args) {
+                launch(args);
+                // Scanner in = new Scanner(System.in);
+                // System.out.println("Input an integer between 1 & 9 (inclusive)");
+                //  HashMap<String, String> question1 = new HashMap<String, String>();
+                // HashMap<String, String> question2 = new HashMap<String, String>();
+                // HashMap<String, String> question3 = new HashMap<String, String>();
+                // HashMap<String, String> question4 = new HashMap<String, String>();
+                // HashMap<String, String> question5 = new HashMap<String, String>();
+                // HashMap<String, String> question6 = new HashMap<String, String>();
+                // HashMap<String, String> question7 = new HashMap<String, String>();
+                // HashMap<String, String> question8 = new HashMap<String, String>();
+                // HashMap<String, String> question9 = new HashMap<String, String>();
+                // question1.put("question", "We asked one student, name a superhero without superpowers. ");
+                // question1.put("answers", "batman 8");
+                // question1.put("answers", "black widow 7");
+                // question1.put("answers", "hawkeye 6");
+                // question1.put("answers", "daredevil 5");
+                // question1.put("answers", "green arrow 4");
+                // question1.put("answers", "iron man 3");
+                // question1.put("answers", "robin 2");
+                // question1.put("answers", "ant man 1");
+                // question2.put("question", "We asked 1 student, name a movie that made over 1 billion at the box office. ");
+                // question3.put("question", "We asked 1 student, name a school subject that’s universally loved. ");
+                // question4.put("question", "We asked 1 student, name a desired vacation city. ");
+                // question5.put("question", "We asked 1 student, name a fictional weapon they’d like to hold. ");
+                // question6.put("question", "We asked 1 student: What is the most useless part of the human body. ");
+                // question7.put("question", "We asked 5 students: What is the most annoying meme? ");
+                // question8.put("question", "We asked 1 student: Name a video game that evolved the field of gaming. ");
+                // question9.put("question", "We asked 1 student: Name the most niche superpower you know. ");
+                // HashMap<Integer, HashMap<String, String>> questions = new HashMap<Integer, HashMap<String, String>>();
+                // questions.put(1, question1);
+                // int ques;
+                // while (!(in.hasNextInt() && ((ques = in.nextInt()) >= 1 && ques <= 9))) {
+                //  System.out.println("Invalid input, try again.");      
+                //  in.nextLine();
+                // }
+                
+                
+ /**
+        
+        *Graphics (Board with eight answers, points counter, timer 30->0, and a question)
+        *Question 1) do today:
+        *counter must accumulate points, in case player loses (total points + x points)
+        *must result in strike if timer runs out (if time == 0)
+        *incorrect answers result in strike (if (answer) == (answer)correct)(else, incorrect)
+        *three strikes locks the game and reveals all left over answer (if incorrect total == 3)
+        *if player clears all eight answers, recieve a congratulatory message (if totalPoints == 36)
+        *negate capitalization on answer (toLowerCase)
+*/
+	}
+}
+        
